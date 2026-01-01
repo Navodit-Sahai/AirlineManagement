c86eafe81620b3227e1dd4660e4cdf96b541e7cf
Navodit-Sahai <sahainavodit781@gmail.com>
2026-01-01 12:57:34 +0530

    added models airports and airplanes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const { PORT } = require("./config/serverconfig.js");
 const ApiRoutes = require("./routes/index");
+const db=require("./models/index");
+const {Airplane}=require("./models/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +16,12 @@ const setupAndStartServer = async () => {
     app.listen(PORT, () => {
       console.log(`Server started at port ${PORT}`);
     });
+    if(process.env.SYNC_DB){
+        db.sequelize.sync({alter:true});
+    }
+    await Airplane.create({
+        modelNumber:'Bombardier CRJ'
+    })
   } catch (error) {
     console.error("Server start error:", error);
   }
